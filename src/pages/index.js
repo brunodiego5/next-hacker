e9de2b737769dc2cf4b12c86c6a131c820b2c259
Bruno Domingues <bruno_diego5@hotmail.com>
2020-05-26 20:44:04 -0300

    Lesson_15._Handling_Errors_with_Built-in_Error_Page_and_Try__Catch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,37 @@
 import React from "react";
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
 
-function Index() {
-  return <div>hacker next</div>;
+function Index({ stories }) {
+  if (stories.length === 0) {
+    return <Error statusCode={503} />;
+  }
+
+  return (
+    <div>
+      <h1>hacker next</h1>
+      <div>
+        {stories.map((story) => (
+          <h2 key={story.id}>{story.title}</h2>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+Index.getInitialProps = async () => {
+  let stories;
+
+  try {
+    const response = await fetch(
+      "https://node-hnapi.herokuapp.com/news?page=1 "
+    );
+    stories = await response.json();
+  } catch (error) {
+    stories = [];
+  }
+
+  return { stories };
+};
+
 export default Index;
