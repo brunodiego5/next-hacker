c25ed8b52a222d553ff160225f6f591204c0dc16
Bruno Domingues <bruno_diego5@hotmail.com>
2020-05-27 19:46:57 -0300

    Lesson_22._Generating_a_Web_App_Manifest_to_our_P

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,11 +31,50 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en-US">
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="robots" content="noindex, nofollow" />
-          <meta name="viewport" content="width=device-width" />
+          <link rel="manifest" href="/manifest.json" />
+
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="next-hacker" />
+          <meta name="apple-mobile-web-app-title" content="next-hacker" />
+          <meta name="theme-color" content="#f60" />
+          <meta name="msapplication-navbutton-color" content="#f60" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="msapplication-starturl" content="/" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/icons/icon-512x512.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="512x512"
+            href="/icons/icon-512x512.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/icons/icon-192x192.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="192x192"
+            href="/icons/icon-192x192.png"
+          />
         </Head>
         <body>
           <Main />
